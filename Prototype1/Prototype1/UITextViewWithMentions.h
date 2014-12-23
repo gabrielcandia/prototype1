ee525a37bb6278a300948824c5d0836d9656c46b
Gabriel Candia <trunketa@gmail.com>
2014-12-22 23:10:19 -0300

    no message

--- Prototype1/Prototype1/UITextViewWithMentions.h
+++ Prototype1/Prototype1/UITextViewWithMentions.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface UITextViewWithMentions : NSObject <UITextViewDelegate, UITableViewDataSource,
             UITableViewDelegate>{
@@ -17,16 +18,19 @@
                 UITextView *auxTextView;
                 UIView *superView;
                 NSString *characterString;
+                CGSize keyboardSize;
+                CGRect originalTextViewRect;
 }
 
 @property (nonatomic, retain) NSMutableDictionary *mentionsDictionaries;
 @property (nonatomic, retain) NSMutableArray *tableViewObjects;
 @property (nonatomic, retain) UIView *accessoryView;
 
--(void)configureClassForTextView: (UITextView *)textView withSuperView: (UIView *)view;
+- (void)configureClassForTextView: (UITextView *)textView withSuperView: (UIView *)view;
+- (void)keyboardWillShow:(NSNotification *)notification;
 - (void)addMentionListWith: (NSMutableArray *)mentionList forCharacter: (NSString *)character;
 - (void)filtrateMentionsForTextView:(UITextView *)textView withWord:(NSString *)word withMentionCharacter: (NSString *)character;
 - (void)configureInputAccessoryView;
 - (BOOL) isAlphaNumeric: (NSString *)string;
--(void)getRectForInputViewWithThisEntries: (NSInteger)count forTextView: (UITextView *)textView;
+- (void)getRectForInputViewWithThisEntries: (NSInteger)count forTextView: (UITextView *)textView;
 @end
--- Prototype1/Prototype1/UITextViewWithMentions.m
+++ Prototype1/Prototype1/UITextViewWithMentions.m
@@ -13,65 +13,92 @@
 @synthesize tableViewObjects;
 @synthesize accessoryView;
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+static const CGFloat ROW_HEIGHT = 44;
+static const CGFloat NUMBER_OF_ROW_PORTRAIT_IPHONE = 3;
+static const CGFloat NUMBER_OF_ROW_LANDSCAPE_IPHONE = 1;
+static const CGFloat NUMBER_OF_ROW_PORTRAIT_IPAD = 3;
+static const CGFloat NUMBER_OF_ROW_LANDSCAPE_IPAD = 3;
+
 
 -(id)init {
     NSLog(@"Init");
     mentionsDictionaries = [[NSMutableDictionary alloc] init];
     accessoryView = [[UIView alloc] init];
-    [self configureInputAccessoryView];
+    
+    tableV = [[UITableView alloc] initWithFrame:CGRectMake(0.0, 0.0, 0.0, 0.0) style:UITableViewStylePlain];
+    tableV.delegate = self;
+    tableV.dataSource = self;
+    tableV.scrollEnabled = YES;
+    //tableView.hidden = YES;
+    
+    accessoryView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, 0.0, 0.0)];
+    accessoryView.backgroundColor = [UIColor blueColor];
+    [accessoryView addSubview:tableV];
+    //[self configureInputAccessoryView];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(configureInputAccessoryView)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
     return self;
 }
+
 -(void)configureClassForTextView: (UITextView *)textView withSuperView: (UIView *)view {
     [textView setDelegate:self];
     textView.inputAccessoryView = self.accessoryView;
     superView = view;
 }
 
-/*-(id)initWithCoder:(NSCoder *)aDecoder
-{
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}*/
+- (void)keyboardWillShow:(NSNotification *)notification {
+    keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+}
 
 - (void)configureInputAccessoryView {
     // Autocomplete TableView Configuration
-    tableV = [[UITableView alloc] initWithFrame:
-              CGRectMake(0.0, 0.0, 320.0, 132.0) style:UITableViewStylePlain];
-    tableV.delegate = self;
-    tableV.dataSource = self;
-    tableV.scrollEnabled = YES;
-    //tableView.hidden = YES;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.width;
+    CGRect accessFrame;
     
-    CGRect accessFrame = CGRectMake(0.0, 0.0, 320.0, 132.0);
-    accessoryView = [[UIView alloc] initWithFrame:accessFrame];
-    accessoryView.backgroundColor = [UIColor blueColor];
-    [accessoryView addSubview:tableV];
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        if(orientation == 0 || orientation == UIInterfaceOrientationPortrait){ //iPhone Portrait
+            accessFrame = CGRectMake(0.0, 0.0, screenWidth, ROW_HEIGHT*NUMBER_OF_ROW_PORTRAIT_IPHONE);
+        }
+        else if(orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight){ //iPhone Landscape
+            accessFrame = CGRectMake(0.0, 0.0, screenWidth, ROW_HEIGHT*NUMBER_OF_ROW_LANDSCAPE_IPHONE);
+        }
+    }
+    else {
+        
+    }
+    accessoryView.frame = accessFrame;
+    tableV.frame = accessFrame;
 }
 
 -(void)getRectForInputViewWithThisEntries: (NSInteger)count forTextView: (UITextView *)textView{
     CGRect tableViewRect;
     CGRect viewRect;
-    double rowHeight = 44.0;
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-    count = (count > 3)? 3 : count;
+    
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    //CGFloat screenHeight = screenRect.size.height;
+    CGFloat screenWidth = screenRect.size.width;
+    
     if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
         if(orientation == 0 || orientation == UIInterfaceOrientationPortrait){ //iPhone Portrait
-            viewRect = CGRectMake(0.0, (3-count)*rowHeight, 320.0, rowHeight*count);
-            tableViewRect = CGRectMake(0.0, 0.0, 320.0, rowHeight*count);
+            count = (count > NUMBER_OF_ROW_PORTRAIT_IPHONE)? NUMBER_OF_ROW_PORTRAIT_IPHONE : count;
+            viewRect = CGRectMake(0.0, (NUMBER_OF_ROW_PORTRAIT_IPHONE-count)*ROW_HEIGHT, screenWidth, ROW_HEIGHT*count);
+            tableViewRect = CGRectMake(0.0, 0.0, screenWidth, ROW_HEIGHT*count);
+            NSLog(@"Frame, X:%f,Y:%f, Width: %f, Height: %f",viewRect.origin.x, viewRect.origin.y, viewRect.size.width, viewRect.size.height);
         }
         else if(orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight){ //iPhone Landscape
-            viewRect = CGRectMake(0.0, 0.0, 480.0, rowHeight);
-            tableViewRect = CGRectMake(0.0, 0.0, 480.0, rowHeight);
+            count = (count > NUMBER_OF_ROW_LANDSCAPE_IPHONE)? NUMBER_OF_ROW_LANDSCAPE_IPHONE : count;
+            viewRect = CGRectMake(0.0, (NUMBER_OF_ROW_LANDSCAPE_IPHONE-count)*ROW_HEIGHT, screenWidth, ROW_HEIGHT*count);
+            tableViewRect = CGRectMake(0.0, 0.0, screenWidth, ROW_HEIGHT*count);
+            NSLog(@"Frame, X:%f,Y:%f, Width: %f, Height: %f",viewRect.origin.x, viewRect.origin.y, viewRect.size.width, viewRect.size.height);
         }
     }
     else {
@@ -93,21 +120,22 @@
 }
 
 - (void)addMentionListWith: (NSMutableArray *)mentionList forCharacter: (NSString *)character {
-    [mentionsDictionaries setObject:mentionList forKey:character];
+    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"self" ascending:YES];
+    NSMutableArray *mentionListSorted = [[mentionList sortedArrayUsingDescriptors:@[descriptor]] mutableCopy];
+    [mentionsDictionaries setObject:mentionListSorted forKey:character];
 }
 
 - (void)filtrateMentionsForTextView:(UITextView *)textView withWord:(NSString *)word withMentionCharacter: (NSString *)character {
     NSMutableArray *objectsList = [mentionsDictionaries objectForKey:character];
     if(objectsList != nil) {
         characterString = character;
-        NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"self" ascending:YES];
         if(word.length > 0) {
             NSString *filter = [NSString stringWithFormat:@"self BEGINSWITH[cd] '%@'" ,word];
             NSPredicate *predicate = [NSPredicate predicateWithFormat:filter];
-            tableViewObjects = [[[objectsList filteredArrayUsingPredicate:predicate] sortedArrayUsingDescriptors:@[descriptor]] mutableCopy];
+            tableViewObjects = [[objectsList filteredArrayUsingPredicate:predicate] mutableCopy];
         }
         else {
-            tableViewObjects = [[objectsList sortedArrayUsingDescriptors:@[descriptor]] mutableCopy];
+            tableViewObjects = objectsList;
         }
         [self getRectForInputViewWithThisEntries:[tableViewObjects count] forTextView:textView];
     }
@@ -129,132 +157,99 @@
 }
 
 - (BOOL)textViewShouldEndEditing:(UITextView *)textView {
-    return false;
+    return TRUE;
 }
 
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range
  replacementText:(NSString *)text {
     NSLog(@"shouldChangeTextInRange");
-    //NSString *deletedText = [textView.text  substringWithRange:range];
-    //NSString *textWithoutRange = [textView.text stringByReplacingCharactersInRange:range withString:@""];
-    
-    NSInteger position = [textView selectedRange].location;
-    NSString *resultedText = [textView.text stringByReplacingCharactersInRange:range withString:text];
-    NSInteger aux = (text.length > 0)? text.length: -1;
-    NSString *subString = [[NSString alloc] init];
-    if(resultedText.length > (position + aux)) {
-        subString = [resultedText substringToIndex:(position + aux)];
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
     }
     else {
-        subString = resultedText;
-    }
-    tableViewObjects = [[NSMutableArray alloc] init];
-    NSArray *wordList = [subString componentsSeparatedByString:@" "];
-    NSString *lastWord = [wordList objectAtIndex:([wordList count]-1)];
-
-    if(lastWord.length > 0) {
-        NSString *firstCharacter = [lastWord substringToIndex:1];
-        /*if([firstCharacter isEqualToString:@"\n"] && ![lastWord isEqualToString:@"\n"]) {
-            firstCharacter = [lastWord substringWithRange:NSMakeRange(1, 1)];
-        }*/
-        if(![self isAlphaNumeric:firstCharacter]) {
-            [self filtrateMentionsForTextView:textView withWord:[lastWord substringFromIndex:1] withMentionCharacter:firstCharacter];
+        NSInteger position = [textView selectedRange].location;
+        NSString *resultedText = [textView.text stringByReplacingCharactersInRange:range withString:text];
+        NSInteger aux = (text.length > 0)? text.length: -1;
+        NSString *subString = [[NSString alloc] init];
+        if(resultedText.length > (position + aux)) {
+            subString = [resultedText substringToIndex:(position + aux)];
+        }
+        else {
+            subString = resultedText;
+        }
+        tableViewObjects = [[NSMutableArray alloc] init];
+        NSArray *wordList = [subString componentsSeparatedByString:@" "];
+        NSString *lastWord = [wordList objectAtIndex:([wordList count]-1)];
+        
+        if(lastWord.length > 0) {
+            NSString *firstCharacter = [lastWord substringToIndex:1];
+            if(![self isAlphaNumeric:firstCharacter]) {
+                [self filtrateMentionsForTextView:textView withWord:[lastWord substringFromIndex:1] withMentionCharacter:firstCharacter];
+            }
+            else {
+                [self getRectForInputViewWithThisEntries:0 forTextView:textView];
+            }
         }
         else {
             [self getRectForInputViewWithThisEntries:0 forTextView:textView];
         }
+        [tableV reloadData];
+        [self resizeTextView:textView];
+        return true;
     }
-    else {
-        [self getRectForInputViewWithThisEntries:0 forTextView:textView];
-    }
-    [tableV reloadData];
-    return true;
+    
 }
 
 - (void)textViewDidChange:(UITextView *)textView {
     NSLog(@"textViewDidChange");
 }
 
-CGFloat animatedDistance;
-static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
-static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
-static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
-static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 348; //216 Keyboard, 132 Maximum inputaccessory
-static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
-
 - (void)textViewDidBeginEditing:(UITextView *)textView {
     textView.inputAccessoryView.hidden = TRUE;
     auxTextView = textView;
-    
+    originalTextViewRect = textView.frame;
+    [self performSelector :@selector(resizeTextView:) withObject:textView afterDelay:0.1];
+
+}
+
+- (void)resizeTextView: (UITextView *)textView {
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    CGFloat screenWidth = screenRect.size.width;
     CGFloat screenHeight = screenRect.size.height;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    NSInteger count = [tableViewObjects count];
+    NSInteger statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
     
-    [textView setFrame:CGRectMake(0.0, 0.0, 320.0, 50.0)];
-    
-    
-    
-    CGRect textViewRect =
-    [superView.window convertRect:textView.bounds fromView:textView];
-    CGRect viewRect =
-    [superView.window convertRect:textView.bounds fromView:textView];
-    
-    CGFloat midline = textViewRect.origin.y + 0.5 * textViewRect.size.height;
-    CGFloat numerator =
-    midline - viewRect.origin.y
-    - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
-    CGFloat denominator =
-    (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION)
-    * viewRect.size.height;
-    CGFloat heightFraction = numerator / denominator;
-    
-    if (heightFraction < 0.5)
-    {
-        heightFraction = 0.0;
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        if(orientation == 0 || orientation == UIInterfaceOrientationPortrait){ //iPhone Portrait
+            count = (count > NUMBER_OF_ROW_PORTRAIT_IPHONE)? 0 : (NUMBER_OF_ROW_PORTRAIT_IPHONE -count);
+        }
+        else if(orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight){ //iPhone Landscape
+            count = (count > NUMBER_OF_ROW_LANDSCAPE_IPHONE)? 0 : (NUMBER_OF_ROW_LANDSCAPE_IPHONE -count);
+        }
     }
-    else if (heightFraction > 1.0)
-    {
-        heightFraction = 1.0;
+    else {
+        if(orientation == 0 || orientation == UIInterfaceOrientationPortrait){ //iPad Portrait
+            
+        }
+        else if(orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight){ //iPad Landscape
+            
+        }
     }
+    CGFloat sizeObjects = count * ROW_HEIGHT;
     
-    UIInterfaceOrientation orientation =
-    [[UIApplication sharedApplication] statusBarOrientation];
-    if (orientation == UIInterfaceOrientationPortrait ||
-        orientation == UIInterfaceOrientationPortraitUpsideDown)
-    {
-        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
-    }
-    else
-    {
-        animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
-    }
-    
-    CGRect viewFrame = superView.frame;
-    viewFrame.origin.y -= animatedDistance;
-    
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationBeginsFromCurrentState:YES];
-    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
-    
-    [superView setFrame:viewFrame];
-    
-    [UIView commitAnimations];
-    
+    CGRect newTextViewFrame = textView.frame;
+    newTextViewFrame.size.height = screenHeight - keyboardSize.height - statusBarHeight + sizeObjects;
+    newTextViewFrame.origin.y = statusBarHeight;
+    newTextViewFrame.origin.x = 0;
+    textView.frame = newTextViewFrame;
+    [textView setFrame:newTextViewFrame];
+    [[UIApplication sharedApplication].keyWindow addSubview:textView];
 }
 
 - (void)textViewDidEndEditing:(UITextView *)textView {
     auxTextView = nil;
-    
-    CGRect viewFrame = superView.frame;
-    viewFrame.origin.y += animatedDistance;
-    
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationBeginsFromCurrentState:YES];
-    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
-    
-    [superView setFrame:viewFrame];
-    
-    [UIView commitAnimations];
+    textView.frame = originalTextViewRect;
 }
 
 /******* TextField Methods END *******/
@@ -276,7 +271,7 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
     return 1;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSLog(@"numbersOfRows: %d",[tableViewObjects count]);
+    NSLog(@"numbersOfRows: %lu",(unsigned long)[tableViewObjects count]);
     return [tableViewObjects count];
 }
 
@@ -293,6 +288,12 @@ static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
     
     [auxTextView replaceRange:textRange withText:[NSString stringWithFormat:@"%@%@ ",characterString, [tableViewObjects objectAtIndex:[indexPath row]]]];
     auxTextView.inputAccessoryView.hidden = YES;
+    tableViewObjects = [[NSMutableArray alloc] init];
+    [self resizeTextView:auxTextView];
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath: (NSIndexPath *) indexPath {
+    return ROW_HEIGHT;
 }
 /******* TableView Methods END *******/
 
